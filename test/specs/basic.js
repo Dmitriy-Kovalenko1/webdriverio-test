9d5a60cd01da08253360e3de5984641ef278ecbd
Dmitriy Kovalenko <qa_19@outlook.com>
2020-01-16 16:58:30 -0800

    Initial commit

---
+++ test/specs/basic.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+
+describe('Register page', () => {
+    it('should have the right title', () => {
+        browser.url('https://stage.pasv.us/user/register')
+        const title = browser.getTitle()
+        assert.strictEqual(title, 'Progress Monitor')
+    })
+   // it('should have a correct title', () => {
+     //   const actual = $('h1').getText();
+       // const expected = 'User Register';
+        //assert(actualH1Text, expectedH1Text);
+    //});
+    //it('should have a correct description', () => {
+     //   const actual = $('h1').getText();
+     //   const expected = 'Profiles with fictitious or dummy data will be deleted';
+     //   assert(actualH1Text, expectedH1Text);
+    //});
+})
+
