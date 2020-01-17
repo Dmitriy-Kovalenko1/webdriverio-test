cedde5510b464f0cd99457b8dd2595d6fa770074
Dmitriy Kovalenko <qa_19@outlook.com>
2020-01-16 23:01:32 -0800

    WDIO Login tests

--- test/specs/register.js
+++ test/specs/register.js
@@ -1,20 +1,111 @@
-const assert = require('assert')
-
+const {expect} = require('chai');
+const email = Math.random() + 'dmitriy@gmail.com';
+const firstName = 'Dmitriy';
+const lastName = 'Kovalenko';
+const password = '123456';
 describe('Register page', () => {
-    it('should have the right title', () => {
+    before(() => {
         browser.url('https://stage.pasv.us/user/register')
-        const title = browser.getTitle()
-        assert.strictEqual(title, 'Progress Monitor')
+    })
+    it('should have the right title', () => {
+        const actual = browser.getTitle();
+        const expected = 'Progress Monitor';
+        expect(actual).equal(expected);
     })
     it('should have a correct title', () => {
         const actual = $('h1').getText();
         const expected = 'User Register';
-        assert(actual, expected);
-    });
-    //it('should have a correct description', () => {
-     //   const actual = $('h1').getText();
-     //   const expected = 'Profiles with fictitious or dummy data will be deleted';
-     //   assert(actualH1Text, expectedH1Text);
-    //});
+        expect(actual).equal(expected);
+    })
+    it('should have a correct description', () => {
+        const actual = $('p').getText();
+        const expected = 'Profiles with fictitious or dummy data will be deleted.';
+        expect(actual).equal(expected);
+    })
+    it('should have a correct submit button text', () => {
+        const actual = $('form button').getText();
+        const expected = 'Submit';
+        expect(actual).equal(expected);
+    })
+    it('should fill First Name field', () => {
+        const element = $('form input[name="firstName"]');
+        element.setValue(firstName);
+        browser.pause(1000);
+    })
+    it('should fill Last Name field', () => {
+        const element = $('form input[name="lastName"]');
+        element.setValue(lastName);
+        browser.pause(1000);
+    })
+    it('should fill Cell phone number field', () => {
+        const element = $('form input[name="phone"]');
+        element.setValue('17775551122');
+        browser.pause(1000);
+    })
+    it('should fill Email field', () => {
+        const element = $('form input[name="email"]');
+        element.setValue(email);
+        browser.pause(1000);
+    })
+    it('should fill Password field', () => {
+        const element = $('form input[name="password"]');
+        element.setValue(password);
+        browser.pause(1000);
+    })
+    it('should fill About field', () => {
+        const element = $('form textarea[name="about"]');
+        element.setValue('fweklnfe kwefnwleknlA wkfNwkqfnqwf.');
+        browser.pause(1000);
+    })
+    it('should fill My goals field', () => {
+        const element = $('form textarea[name="goals"]');
+        element.setValue('Iwefb;qwkfnowkefn weknfqekle wfknewlqfnqadada ssfcswcwcv.');
+        browser.pause(1000);
+    })
+    it('should choose English level dropdown', () => {
+        const element = $('form select[name="englishLevel"]');
+        element.selectByVisibleText('Advanced');
+        browser.pause(1000);
+    })
+    it('should click button', () => {
+        const element = $('form button[type="submit"]');
+        element.click();
+        browser.pause(1000);
+    })
 })
-
+describe('Login page', () => {
+    before(() => {
+        browser.url('https://stage.pasv.us/user/login')
+    })
+    it('should fill Email field', () => {
+        const element = $('form input[name="email"]');
+        element.setValue(email);
+        browser.pause(1000);
+    })
+    it('should fill Password field', () => {
+        const element = $('form input[name="password"]');
+        element.setValue(password);
+        browser.pause(1000);
+    })
+    it('should click button', () => {
+        const element = $('form button[type="submit"]');
+        element.click();
+        browser.pause(1000);
+    })
+    it('should have a correct part of link', () => {
+        const link = $('*=Dmitriy Kovalenko');
+        link.getText();
+        browser.pause(2000);
+    });
+});
+describe('Home page', () => {
+    before(() => {
+        browser.url('https://stage.pasv.us')
+    })
+    it('should have the right title', () => {
+        const actual = browser.getTitle();
+        const expected = 'Progress Monitor';
+        expect(actual).equal(expected);
+        browser.pause(1000);
+    })
+});
